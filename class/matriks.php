4246c76650fe5ff4cafe6ce7d3a0f8988ee6090e
ramdan riawan <ramdanriawan3@gmail.com>
2018-01-07 01:22:31 +0700

    tambah,kurang,kali,transpose

--- class/matriks.php
+++ class/matriks.php
@@ -27,9 +27,10 @@ class matriks extends matriksError
 
       $this->matriksIsiData($matriks1);
       $this->matriksIsiData($matriks2);
-      parent::matriksErrorJumlahBaris($matriks1, $matriks2);
-      parent::matriksErrorJumlahElementBaris($matriks1, $matriks2);
-      parent::matriksErrorElementBukanNumeric($matriks1, $matriks2);
+      parent::matriksErrorElementBukanNumeric($matriks1);
+      parent::matriksErrorElementBukanNumeric($matriks2);
+      parent::matriksErrorJumlahBarisDuaMatriks($matriks1, $matriks2);
+      parent::matriksErrorJumlahElementBarisDuaMatriks($matriks1, $matriks2);
 
       //foreach untuk menambah setiap baris element array menjadi array baru
       foreach ($matriks1 as $key => $value) {
@@ -50,9 +51,10 @@ class matriks extends matriksError
 
       $this->matriksIsiData($matriks1);
       $this->matriksIsiData($matriks2);
-      parent::matriksErrorJumlahBaris($matriks1, $matriks2);
-      parent::matriksErrorJumlahElementBaris($matriks1, $matriks2);
-      parent::matriksErrorElementBukanNumeric($matriks1, $matriks2);
+      parent::matriksErrorElementBukanNumeric($matriks1);
+      parent::matriksErrorElementBukanNumeric($matriks2);
+      parent::matriksErrorJumlahBarisDuaMatriks($matriks1, $matriks2);
+      parent::matriksErrorJumlahElementBarisDuaMatriks($matriks1, $matriks2);
 
       //foreach untuk menambah setiap baris element array menjadi array baru
       foreach ($matriks1 as $key => $value) {
@@ -99,17 +101,141 @@ class matriks extends matriksError
       return $matriksHasil;
    }
 
+   //fungsi untuk mendapatkan matriks berdasarkan column
+   function matriksGetColumn(array $matriks1, int $columnKe)
+   {
+      $matriksHasil = [];
+      // $this->matriksIsiData($matriks1);
+
+      foreach ($matriks1 as $key => $value) {
+         if(array_key_exists($columnKe, $matriks1[$key]))
+         {
+            $matriksHasil[] = $matriks1[$key][$columnKe];
+         }else
+         {
+            break;
+         }
+      }
+
+      if(count($matriksHasil) < 1)
+      {
+         return false;
+      }else
+      {
+         return $matriksHasil;
+      }
+   }
+
+   //fungsi untuk mendapatkan matriks berdasarkan baris
+   function matriksGetBaris(array $matriks1, int $barisKe)
+   {
+      $matriksHasil = [];
+      $matriks1Keys = array_keys($matriks1);
+
+      if(array_key_exists($barisKe, $matriks1Keys))
+      {
+         return $matriksHasil = $matriks1[$matriks1Keys[$barisKe]];
+      }else
+      {
+         return false;
+      }
+   }
+
+   //fungsi untuk menyimpan perkalian antar baris dan column
+   function matriksOperasiKaliBarisDanColumn(array $matriksBaris, array $matriksColumn) : int
+   {
+      $penjumlahan = 0;
+
+      foreach ($matriksBaris as $key => $value) {
+         $penjumlahan += $matriksBaris[$key] * $matriksColumn[$key];
+      }
+
+      return $penjumlahan;
+   }
+
    function matriksOperasiKali(array $matriks1, array $matriks2) : array
    {
-      $arrayHasil = [];
+      $matriksHasil = [];
+      $this->matriksIsiData($matriks1);
+      $this->matriksIsiData($matriks2);
+      parent::matriksErrorElementBukanNumeric($matriks1);
+      parent::matriksErrorElementBukanNumeric($matriks2);
+      parent::matriksErrorCekBarisDanColumnDuaMatriks($matriks1, $matriks2);
 
+      //list keys dari kedua matriks (ambil yang $matriks1 saja)
+      $matriksKeys          = array_keys($matriks1);
 
-      return $arrayHasil;
+      //jumlah baris matriks1 untuk membuaat perulangan sebanyak jumlah barisnya
+      $jumlahBarisMatriks1  = count($matriks1);
+
+      //jumlah column matriks2 untuk menjumlahkan tiap baris ke tiap column sesuai keys yang telah di list di atas
+      $jumlahColumnMatriks2 = count($matriks2[$matriksKeys[0]]);
+
+      for ($i=0; $i < $jumlahColumnMatriks2; $i++) {
+
+         for ($j=0; $j < $jumlahBarisMatriks1; $j++) {
+            $matriksBaris  = $this->matriksGetBaris($matriks1, $j);
+            $matriksColumn = $this->matriksGetColumn($matriks2, $i);
+            $penjumlahan   = $this->matriksOperasiKaliBarisDanColumn($matriksBaris, $matriksColumn);
+
+            //simpan hasil penjumlahannya ke dalam setiap baris dan column
+            $matriksHasil[$matriksKeys[$j]][] = $penjumlahan;
+
+            //reset ke 0 untuk menghitung jumlah perkalian dengan column lainnya lagi
+            $penjumlahan = 0;
+         }
+      }
+
+      return $matriksHasil;
+   }
+
+   //khusus untuk operasi matriks kali all, semua jumlah baris dan column matriks 1 harus sama dengan semua jumlah baris dan matriks 2
+   function matriksOperasiKaliAll(array $matriks) : array
+   {
+      $matriksHasil = [];
+
+      for ($i=0; $i < count($matriks) - 1; $i++) {
+         $matriks1 = $matriks[$i];
+         $matriks2 = $matriks[$i + 1];
+         $matriks[$i + 1] = $this->matriksOperasiKali($matriks1, $matriks2);
+         $matriksHasil = $matriks[$i + 1];
+      }
+
+      return $matriksHasil;
    }
 
    function matriksTranspose(array $matriks1) : array
    {
       $this->matriksIsiData($matriks1);
+      parent::matriksErrorCekBarisDanColumn($matriks1);
+      $matriksHasil = [];
+
+      $i = 0;
+      foreach ($matriks1 as $key => $value) {
+
+         foreach ($matriks1 as $key2 => $value2) {
+            //lakukan pertukaran setiap element pada baris dengan setiap element pada column
+            //tergantung $i
+
+            $matriksHasil[$key][] = $matriks1[$key2][$i];
+         }
+
+         //ubah setiap column
+         $i++;
+      }
+
+      return $matriksHasil;
+   }
+
+   function matriksTransposeAll(array $matriks1) : array
+   {
+      $matriksHasil = [];
+
+      foreach ($matriks1 as $key => $value) {
+         $matriksHasil[] = $this->matriksTranspose($value);
+      }
+
+      return $matriksHasil;
    }
 
 }
@@ -117,29 +243,45 @@ class matriks extends matriksError
 $matriks = new matriks();
 
 
+
+
 $matriks1 = array(
-   "b1" => "1;2;4;4",
-   "b2" => "2;3;4;5",
-   "b3" => "2;3;4;5"
+   "b1" => "1;2;3;4", //1,2,3,7
+   "b2" => "2;3;4;5", //2,3,4,8
+   "b3" => "3;4;5;6", //3,4,5,9
+   "b4" => "4;5;6;7" //4,5,6,10
 );
 
 $matriks2 = array(
-   "b1" => "1;2;3;4",
-   "b2" => "2;3;4;5",
-   "b3" => "2;3;4;5"
+   "b1" => "1;2;3;4", //1,2,3,7
+   "b2" => "2;3;4;5", //2,3,4,8
+   "b3" => "3;4;5;6", //3,4,5,9
+   "b4" => "3;4;5;6", //3,4,5,9
 );
 
+
 $matriks3 = array(
-   "b1" => "1;2;3;4",
-   "b2" => "2;3;4;5",
-   "b3" => "2;3;4;5"
+   "b1" => "1;2;3;4", //1,2,3,7
+   "b2" => "2;3;4;5", //2,3,4,8
+   "b3" => "3;4;5;6", //3,4,5,9
+   "b4" => "4;5;6;7" //4,5,6,10
 );
 
 $matriks4 = array(
-   "b1" => "1;2;3;4",
-   "b2" => "2;3;4;5",
-   "b3" => "2;3;4;5"
+   "b1" => "1;2;3;4", //1,2,3,7
+   "b2" => "2;3;4;5", //2,3,4,8
+   "b3" => "3;4;5;6", //3,4,5,9
+   "b4" => "7;8;9;10" //4,5,6,10
 );
 
+$matriks5 = array(
+   "b1" => "1;2;3;4", //1,2,3,7
+   "b2" => "2;3;4;5", //2,3,4,8
+   "b3" => "3;4;5;6", //3,4,5,9
+   "b4" => "7;8;9;10" //4,5,6,10
+);
+
+echo "<pre>";
+print_r($matriks->matriksOperasiKaliAll(array($matriks1, $matriks2, $matriks3, $matriks4, $matriks5)));
 
 ?>
--- class/matriksError.php
+++ class/matriksError.php
@@ -2,44 +2,74 @@
 
 class matriksError
 {
-   function matriksErrorJumlahBaris(array $matriks1, array $matriks2)
+   function matriksErrorJumlahBarisDuaMatriks(array $matriks1, array $matriks2)
    {
       //foreach untuk mengecek apakah jumlah baris setiap element array sama atau tidak
       foreach($matriks1 as $key => $value)
       {
          if(count($matriks1) != count($matriks2))
          {
-            print_r(array("error" => "Jumlah baris tidak sama!"));
+            echo "Jumlah baris tidak sama!";
             die();
          }
       }
    }
 
-   function matriksErrorJumlahElementBaris(array $matriks1, array $matriks2)
+   function matriksErrorJumlahElementBarisDuaMatriks(array $matriks1, array $matriks2)
    {
       //foreach untuk mengecek jumlah element setiap element array sama atau tidak
       foreach ($matriks1 as $key => $value) {
          foreach($value as $key2 => $value2)
          {
-            if((!is_numeric($matriks1[$key][$key2]) || !is_numeric($matriks2[$key][$key2])) || (count($matriks1[$key]) != count($matriks2[$key]))) // pastikan juga tidak ada element yang kosong
+            if(count($matriks1[$key]) != count($matriks2[$key])) //jika jumlah element tiap baris tidak sama
             {
-               print_r(array("error" => "Jumlah element setiap baris tidak sama!"));
+               echo "Jumlah element setiap baris tidak sama!";
                die();
             }
          }
       }
    }
 
-   function matriksErrorElementBukanNumeric(array $matriks1, array $matriks2)
+   function matriksErrorElementBukanNumeric(array $matriks1)
    {
       foreach ($matriks1 as $key => $value) {
          foreach($value as $key2 => $value2)
          {
-            if(!is_numeric($matriks1[$key][$key2]) || !is_numeric($matriks2[$key][$key2]))
+            if(!is_numeric($matriks1[$key][$key2]))
             {
-               print_r(array("error" => "Beberapa element baris yang di inputkan bukan numeric!"));
+               echo "Beberapa element baris yang di inputkan bukan numeric!";
                die();
             }
+         }
+      }
+   }
+
+   // function matriksErrorCekBarisDanColumn(array $matriks1)
+   // {
+   //    $jumlahBaris = count($matriks1);
+   //
+   //    foreach ($matriks1 as $key => $value) {
+   //       $jumlahColumn = count($matriks1[$key]);
+   //
+   //       if($jumlahBaris != $jumlahColumn)
+   //       {
+   //          echo "Jumlah baris dan column di suatu matriks tidak sama!";
+   //          die();
+   //       }
+   //    }
+   // }
+
+   function matriksErrorCekBarisDanColumnDuaMatriks($matriks1, $matriks2)
+   {
+      $jumlahBarisMatriks2 = count($matriks2);
+
+      foreach ($matriks1 as $key => $value) {
+         $jumlahColumnMatriks1 = count($matriks1[$key]);
+
+         if($jumlahColumnMatriks1 != $jumlahBarisMatriks2)
+         {
+            echo "Antara jumlah baris dengan jumlah column dari 2 matrik tidak sama";
+            die();
          }
       }
    }
